c34ae35f464c8a60cecbc033fb3b69ba05405290
ArmondBane <gohmsterwb@gmail.com>
2021-03-29 03:08:45 +0300

    Add Coil Bitmap Converter

--- app/src/main/java/com/example/moviediary/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviediary/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.moviediary.ui
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.example.moviediary.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,5 +17,15 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    private suspend fun getBitmap(): Bitmap {
+        val loading = ImageLoader(this)
+        val request = ImageRequest.Builder(this)
+            .data("https://avatars3.githubusercontent.com/u/14994036?s=400&u=2832879700f03d4b37ae1c09645352a352b9d2d0&v=4")
+            .build()
+
+        val result = (loading.execute(request) as SuccessResult).drawable
+        return (result as BitmapDrawable).bitmap
     }
 }
